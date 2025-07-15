699e0cce8a64715bc9e8c0f1732696ae48f55649
kamrul-nova-06 <kamrulislam2007s@gmail.com>
2025-07-15 16:11:05 +0600

    Update server.js

--- server.js
+++ server.js
@@ -7,18 +7,22 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', (socket) => {
-    console.log('A user connected');
+  console.log('A user connected');
 
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
-    });
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
 
-    socket.on('disconnect', () => {
-        console.log('A user disconnected');
-    });
+  socket.on('chat image', (msg) => {
+    io.emit('chat image', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
 });
 
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
