73078d6d9d9a9616d400a18a545e7d2c1b5f7efb
kamrul-nova-06 <kamrulislam2007s@gmail.com>
2025-07-15 22:54:35 +0600

    Update notify.js

--- public/notify.js
+++ public/notify.js
@@ -1,23 +1,4 @@
-// Notification permission request
-if ('Notification' in window && Notification.permission !== 'granted') {
-  Notification.requestPermission().then(function(permission) {
-    console.log("Notification permission:", permission);
-  });
-}
-
-// Notification sound function
 function playNotification() {
-  const audio = new Audio('/notify.mp3'); // তুমি চাইলে এখানে base64 sound ব্যবহার করতে পারো
-  audio.play();
-}
-
-// Show notification (only when user is not on page)
-function showNotification(title, body) {
-  if (document.hidden && Notification.permission === 'granted') {
-    new Notification(title, {
-      body: body,
-      icon: '/chat-icon.png' // তোমার PNG ফাইলের নাম এখানে
-    });
-    playNotification();
-  }
+  const audio = new Audio('/notify.mp3');
+  audio.play().catch(() => {});
 }
